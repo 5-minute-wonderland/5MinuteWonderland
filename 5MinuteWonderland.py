b05082c60d1354036992fcdf2b522a77fdf2d4a2
MobGrindathon <matthew.stahel@gmail.com>
2025-03-29 12:22:55 -0500

    Basic Code Update

---
+++ 5MinuteWonderland.py
@@ -0,0 +1,31 @@
+import sys
+import socket
+import multiprocessing
+import numpy as np
+import ipaddress
+
+UDP_ListenPort = 6969
+UDP_SendPort = 8794
+network = ipaddress.ip_network
+
+# Scan for Devices
+def NetworkSearch():
+    return
+
+# Host Game
+
+# Find Host
+def FindHost():
+    return
+# Send packets
+def SendPackets():
+    return
+
+# main method
+if __name__=="__main__":
+    response = input("Would You like to host or join a game?: ")
+    print(network)
+    while response.upper() != "HOST" and response.upper() != "JOIN":
+        response = input("Invalid response: Type \"Host\" to host or \"Join\" to join ")
+        print(network)
+    exit
